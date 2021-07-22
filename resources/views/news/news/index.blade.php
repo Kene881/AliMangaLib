ce9b3c439beeb2817601598486b244cd4865f086
Gleb <asd>
2021-07-22 17:51:13 +0600

    рабочая версия

--- resources/views/news/news/index.blade.php
+++ resources/views/news/news/index.blade.php
@@ -1,51 +1,66 @@
-<x-layouts.app :title="__('News')">
+<x-layouts.navbar :title="__('News')">
 
-    <a class="btn btn-primary" href="{{ route('news.news.create') }}">
-        {{__('Add news')}}
-    </a>
+    <div class="container-fluid">
+        <div class="row news-start-container d-flex justify-content-start">
 
-    @if($news->isEmpty())
-        <div class="alert alert-secondary">
-            {{__('No news added yet')}}
-            {{__('Please, ')}}
-            <a class="alert-link" href="{{ route('news.news.create') }}">
-                {{__('add one')}}
-            </a>
-        </div>
-    @else
-
-        @foreach($news as $one_news)
-
-            <div class="card card-body my-3 d-flex flex-row align-items-center">
-
-                <div class="me-auto">
-                    {{ $one_news->title }}
-                </div>
-
-                <form action="{{ route('news.news.destroy', $one_news) }}" method="post">
-                    @csrf @method('delete')
-
-                    <div class="btn-group btn-group-sm">
-                        <a class="btn btn-primary" href="{{ route('news.news.show', $one_news) }}">
-                            {{__('View')}}
-                        </a>
-                        <a class="btn btn-warning" href="{{ route('news.news.edit', $one_news) }}">
-                            {{__('Edit')}}
-                        </a>
-
-                        <button class="btn btn-danger">
-                            {{__('Delete')}}
-                        </button>
+            <div class="col-md-10">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-auto">
+                        <div style="height: 30px;width: 30px;">
+                            <img class="rounded float-left fullWHImage" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMS6m2Iz5lXmol7S20vFcLVNc2IZUhBSAX3Q&usqp=CAU"  alt="...">
+                        </div>
                     </div>
 
-                </form>
-
+                    <div class="col-md-2">
+                        <h3>News</h3>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="container" style="width: 35.5%;">
+            <div class="row mt-4">
+                <div class="col-md-12">
+                    <a class="cancel-button btn" href="{{ route('news.news.create') }}">{{ __('Create') }}</a>
+                </div>
+            </div>
+        </div>
+        {{--News and icon--}}
 
+        @if($news->isEmpty())
+            <div class="alert alert-secondary">
+                {{__('No news added yet')}}
+                {{__('Please, ')}}
+                <a class="alert-link" href="{{ route('news.news.create') }}">
+                    {{__('add one')}}
+                </a>
+            </div>
+        @else
+
+            @foreach($news as $one_news)
+
+                <div class="row news-header-container d-flex justify-content-center mb-2">
+                    <div class="col-md-4 p-3 news-post">
+                        <div class="row news-post-sub-info p-1">
+
+                            <div class="col-md-2">
+                                <span> {{$one_news->created_at->format('d/m/Y')}} </span>
+                            </div>
+
+                        </div>
+
+                        <div class="row mt-2" style="letter-spacing: 2px;">
+                            <a href="{{ route('news.news.show', $one_news) }}" class="h5">{{$one_news->title}}</a>
+                        </div>
+                    </div>
+                </div>
+                {{--Post in news--}}
         @endforeach
 
         {{ $news->links() }}
 
-    @endif
+        @endif
+    </div>
 
-</x-layouts.app>
+
+</x-layouts.navbar>
+
