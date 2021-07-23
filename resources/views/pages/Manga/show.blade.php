5efb52e022e8a43058af5326cbe441ca85b320cf
Kene881 <55137924+Kene881@users.noreply.github.com>
2021-07-23 11:01:57 +0600

    changed show in manga

--- resources/views/pages/Manga/show.blade.php
+++ resources/views/pages/Manga/show.blade.php
@@ -9,11 +9,11 @@
     <div class="row d-flex justify-content-start mt-3 manga-header-info">
         <div class="col-md-12">
             <div class="row">
-                <div class="col-md-4">
-                    <img style="width: 100%; height: 25vw;" class="rounded" src="{{ \Storage::url($manga->image_path) }}">
+                <div class="col-md-3">
+                    <img style="width: 100%; height: 22vw;" class="rounded" src="{{ \Storage::url($manga->image_path) }}">
                 </div>
 
-                <div class="col-md-8">
+                <div class="col-md-9">
                     <div class="card m-1">
                         <div class="card-footer">
                             <div class="row border rounded border-dark m-1 p-1">
@@ -44,36 +44,38 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+{{--                    --}}
+                    <div>
+                        <form action="{{ route('manga.destroy', $manga) }}" method="post">
+                            @csrf @method('delete')
 
+                            <div class="row mt-4">
+                                <div class="col-md-6 float-left">
+                                    <a href="{{ route('manga.edit', $manga) }}" class="btn create-button">{{ __('EDIT') }}</a>
+                                </div>
 
-            <div>
+                                <div class="col-md-6 float-left">
+                                    <button class="btn create-button">
+                                        DELETE
+                                    </button>
+                                </div>
+                            </div>
 
-                <form action="{{ route('manga.destroy', $manga) }}" method="post">
-                    @csrf @method('delete')
-
-                    <div class="row mt-4">
-                        <div class="col-md-6 float-left">
-                            <a href="{{ route('manga.edit', $manga) }}" class="btn create-button">{{ __('EDIT') }}</a>
-                        </div>
-
-                        <div class="col-md-6 float-left">
-                            <button class="btn create-button">
-                                DELETE
-                            </button>
-                        </div>
+                        </form>
                     </div>
 
-                </form>
-            </div>
 
-
-            <div class="row mt-4">
-                <div class="col-md-12">
-                    <a href="{{ route('chapter.index', ['manga' => $manga]) }}" class="btn create-button">Go to chapters</a>
+                    <div class="row mt-4">
+                        <div class="col-md-12">
+                            <a href="{{ route('chapter.index', ['manga' => $manga]) }}" class="btn create-button">Go to chapters</a>
+                        </div>
+                    </div>
+{{--                    --}}
                 </div>
             </div>
+
+
+
 
         </div>
     </div>
